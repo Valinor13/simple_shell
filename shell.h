e9087f49e10e68120d1619b13001a22ddf90c5b7
Christopher-Caswell <christopher.caswell@rocketmail.com>
2021-04-02 20:51:25 +0000

    Init header

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+void print_prompt1(void);
+void print_prompt2(void);
+char *read_cmd(void);
+
+#endif
