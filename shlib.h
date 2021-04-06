aaa1761fae50fc0556c6964626f4f353ae6a212e
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-06 18:16:19 +0000

    Recommit without unused files and functions

--- shlib.h
+++ shlib.h
@@ -8,22 +8,6 @@
 #include <limits.h>
 #include <unistd.h>
 
-typedef struct source_s
-{
-	char *buffer;
-	long buffsize;
-	long charpo;
-} sauce_t;
-
-typedef struct token_s
-{
-	sauce_t *ptr;
-	int tlen;
-	char *txt;
-} token_t;
-
-extern struct token_s eof_token;
-
 void print_prompt1(void);
 void print_prompt2(void);
 char *read_cmd(void);
@@ -34,12 +18,6 @@ void _puts(char *str);
 size_t _strlen(char *s);
 char *_realloc(char *ptr, size_t olen, size_t nlen);
 char *_strcpy(char *dest, char *src, size_t ptrlen);
-char next_char(sauce_t *ptr);
-void unget_char(sauce_t *ptr);
-char peek_char(sauce_t *ptr);
-void skip_white_spaces(sauce_t *ptr);
-token_t *tokenize(sauce_t *ptr);
-void free_token(token_t *tkn);
 size_t get_tkncnt(char *str);
 char *_strdup(char *str);
 
