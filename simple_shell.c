7e1ef88dd83554cb42e788463126ccf2366d31d0
Jay Calhoun <jwcalhoun2@gmail.com>
2021-04-09 15:10:59 +0000

    Implementing PATH step one

--- simple_shell.c
+++ simple_shell.c
@@ -4,12 +4,14 @@
  * main - simple shell
  * Return: returns 1 for success -1 for failure
  */
-int main(void)
+int main(int ac, char *av[], char *env[])
 {
 	char *cmd = NULL, **tknptr = NULL, *cntptr = NULL, *tmp = NULL;
 	size_t tkncnt, i, mode;
 
-	mode = isatty(STDIN_FILENO);		
+	(void)ac;
+	(void)env;
+	mode = isatty(STDIN_FILENO);
 	while (1)
 	{
 		if (mode)
@@ -22,7 +24,7 @@ int main(void)
 			free(cmd);
 			continue;
 		}
-		if (_strcmp(cmd, "exit") == 0)
+		if (_strcmp(cmd, "exit\0") == 0)
 		{
 			free(cmd);
 			break;
@@ -35,15 +37,14 @@ int main(void)
 		for (i = 1; i < tkncnt - 1; i++)
 			tknptr[i] = strtok(NULL, " ");
 		tknptr[i] = NULL;
-		if (_strcmp("/bin/", tknptr[0]) == 0)
-			tmp = NULL, get_exec(tknptr, cmd, tmp);
-		else
+/*		if (_strcmp("./", tknptr[0]) == 0)
+*/		tmp = NULL, _exec(tknptr, cmd, tmp, av, env);
+/*		else
 		{
-			tmp = _strcat("/bin/", tknptr[0]);
-			tknptr[0] = tmp;
-			get_exec(tknptr, cmd, tmp);
-		}	
-		if (tmp != NULL)
+			tmp = _strcat("./", tknptr[0]);
+			tknptr[0] = tmp, _exec(tknptr, cmd, tmp, av, env);
+		}
+*/		if (tmp != NULL)
 			free(tmp);
 		free(tknptr), free(cmd);
 		if (mode == 0)
@@ -53,11 +54,13 @@ exit(1);
 }
 
 /**
- * get_exec - executes a command
+ * _exec - executes a command
  * @tknptr: input array of strings
+ * @cmd: input cmd pointer
+ * @tmp: input tmp pointer
  * Return: returns void
  */
-void get_exec(char **tknptr, char *cmd, char *tmp)
+void _exec(char **tknptr, char *cmd, char *tmp, char *av[], char *env[])
 {
 	pid_t pid = fork();
 
@@ -66,12 +69,12 @@ void get_exec(char **tknptr, char *cmd, char *tmp)
 	wait(NULL);
 	if (pid == 0)
 	{
-		if (execve(tknptr[0], tknptr, NULL) == -1)
+		if (execve(tknptr[0], tknptr, env) == -1)
 		{
 			if (tmp != NULL)
 				free(tmp);
 			free(cmd), free(tknptr);
-			perror("./hsh");
+			perror(av[0]);
 			exit(1);
 		}
 	}
